2fa4fc165c5bbbfa313a7b4176456472e5fcd8ab
LegendKan <kanchuanqi@gmail.com>
2016-08-20 11:09:28 +0800

    add assets

---
+++ Assets/Scripts/Camera/FollowCameraControl.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FollowCameraControl : MonoBehaviour {
+
+	public Vector3 offsetPosition;
+
+	//[HideInInspector] public Transform targetTransform;
+	[HideInInspector] public TankManager tankManager;
+	[HideInInspector] public Text m_MessageText;
+
+
+	private void Start ()
+	{
+		
+	}
+
+
+	private void Update ()
+	{
+		transform.position = offsetPosition + tankManager.m_Instance.transform.position;
+		if (!tankManager.m_Instance.activeSelf) {
+			Debug.Log ("Should show canvas");
+			m_MessageText.text = DeadMessage ();
+			//m_MessageText.text = "Hello";
+		} else {
+			m_MessageText.text = string.Empty;
+		}
+	}
+
+	public void Active()
+	{
+		gameObject.SetActive(true);
+	}
+
+	public void Disable()
+	{
+		gameObject.SetActive(false);
+	}
+
+	private string DeadMessage()
+	{
+		float deadTime = tankManager.GetDeadTime ();
+		//Debug.Log ("Dead Time "+deadTime);
+		float remaining = tankManager.m_RebornDelay - Time.time + deadTime;
+		int final = Mathf.CeilToInt (remaining);
+		string message = string.Empty;
+		if(remaining>0){
+			message = "等待复活\n" + final + "s";
+		}
+		return message;
+	}
+}
