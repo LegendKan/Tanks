1b1a2c81172d4ff63561f5535e342c7be7564ed2
Ticentxia <ticentxia@gmail.com>
2016-08-25 21:36:14 +0800

    add base movement ai tree

---
+++ Assets/Task/isEnemyDistanceFromprop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class isEnemyDistanceFromprop : Conditional
+{
+    public AIController aiCtrl;
+    public float tank2distanceFromProp;
+    public float tank1distanceFromProp;
+
+    public override void OnStart()
+    {
+        aiCtrl = GetComponent<AIController>();
+    }
+    public override TaskStatus OnUpdate()
+    {
+        tank2distanceFromProp = Vector3.Distance(transform.position, aiCtrl.GetCurrentHealthTransform().position);
+        tank1distanceFromProp = Vector3.Distance(aiCtrl.GetEnemyTransform().position, aiCtrl.GetCurrentHealthTransform().position);
+        if (tank2distanceFromProp > tank1distanceFromProp)
+        {
+            return TaskStatus.Success;
+        }
+        else return TaskStatus.Failure;
+    }
+}
---
+++ Assets/Task/isPropRefresh.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class isPropRefresh : Conditional{
+    public AIController aiCtl;
+    public override void OnStart()
+    {
+        if (aiCtl == null) {
+            aiCtl = GetComponent<AIController>();
+        }
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if (aiCtl.HasHealthBag())
+        {
+            return TaskStatus.Success;
+        }
+        else return TaskStatus.Failure;
+    }
+}
